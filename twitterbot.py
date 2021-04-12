183b089756e77c0f3420e7304f0c3d45100d8fd7
k0gane <50335010+k0gane@users.noreply.github.com>
2021-04-12 12:17:28 +0900

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -851,6 +851,6 @@ def scraping_json(idol_id):
 
 now = datetime.datetime.now()+timedelta(hours=9)
 tweet_picture(now)
-if((0 <= ((now.day-12)*24+now.hour-15)<= 213) and now.minute==20):
+if((1 <= ((now.day-12)*24+now.hour-15)<= 213) and now.minute==20):
     tweet_picture(now)
     
